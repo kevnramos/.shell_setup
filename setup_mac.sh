a7d884da431fefed92e7672a2a5339bf5db3b809
Kevin Ramos <kevramos1121@gmail.com>
2023-03-28 23:52:55 -0400

    remove old files script

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,5 +1,15 @@
 rm `find . -name .DS_Store`
 
+cd mac
+# delete ".*" files in ~ with same name as files in mac/ folder
+for file in .*; do
+    cd ~
+    rm $file
+done
+
+rm -rf p10k_styles/
+echo "Removed files and symlinked new ones...\n"
+
 cd ~
 
 cp -s ~/.shell_setup/common/.* .
--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,5 +1,14 @@
 rm `find . -name .DS_Store`
 
+cd ubuntu
+# delete ".*" files in ~ with same name as files in mac/ folder
+for file in .*; do
+    cd ~
+    rm $file
+done
+
+echo "Removed files and symlinked new ones...\n"
+
 cd ~
 
 cp -s ~/.shell_setup/common/.* .
