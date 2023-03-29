26e208989b842ba772e293e2eb013f3ad05518f0
Kevin Ramos <kevramos1121@gmail.com>
2023-03-29 00:25:43 -0400

    fix rm function

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,12 +2,26 @@ if [ `find . -name .DS_Store` ]; then
     rm `find . -name .DS_Store`
 fi
 
+function delete(){
+    for file in .*; do
+        cd ~
+        rm $file
+    done
+}
+
+CURRENT=`pwd`
+#####################################################################
 cd ubuntu
+# go into ubuntu folder, and loop over hidden files
 # delete ".*" files in ~ with same name as files in mac/ folder
-for file in .*; do
-    cd ~
-    rm $file
-done
+delete
+
+#####################################################################
+cd $CURRENT
+cd common
+# go into common folder, and loop over hidden files
+# delete ".*" files in ~ with same name as files in common/ folder
+delete
 
 echo "Removed files and symlinked new ones...\n"
 
@@ -15,4 +29,3 @@ cd ~
 
 cp -s ~/.shell_setup/common/.* .
 cp -s ~/.shell_setup/ubuntu/.* .
-
