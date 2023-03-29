abc1c4bd4c8012953a6f3c00dee84fd11eccda23
Kevin Ramos <kevramos1121@gmail.com>
2023-03-28 23:36:11 -0400

    scripts for setup for mac & ubuntu

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,8 +1,8 @@
-if [ -f .DS_Store ]; then
-    rm .DS_Store
-fi
+rm `find . -name .DS_Store`
 
 cd ~
 
+
+cp -s ~/.shell_setup/common/.* .
 cp -s ~/.shell_setup/mac/.* .
 ln -s ~/.shell_setup/mac/p10k_styles
---
+++ setup_ubuntu.sh
@@ -0,0 +1,6 @@
+rm `find . -name .DS_Store`
+
+cd ~
+
+cp -s ~/.shell_setup/common/.* .
+cp -s ~/.shell_setup/ubuntu/.* .
